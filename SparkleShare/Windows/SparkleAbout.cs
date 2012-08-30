32557dd479163411ecb79953d0d99d6832fe8079
Nicolas Raoul <nicolas.raoul@gmail.com>
2012-08-30 16:55:37 +0900

    rebranding

--- SparkleShare/Windows/SparkleAbout.cs
+++ SparkleShare/Windows/SparkleAbout.cs
@@ -36,7 +36,7 @@ namespace SparkleShare {
 
         public SparkleAbout ()
         {
-            Title      = "About SparkleShare";
+            Title      = "About CmisSync";
             ResizeMode = ResizeMode.NoResize;
             Height     = 288;
             Width      = 640;
@@ -111,7 +111,7 @@ namespace SparkleShare {
                 Foreground   = new SolidColorBrush (Colors.White),
                 Text         = "Copyright © 2010–" + DateTime.Now.Year + " Hylke Bons and others.\n" +
                     "\n" +
-                    "SparkleShare is Open Source software. You are free to use, modify, " +
+                    "CmisSync is Open Source software. You are free to use, modify, " +
                     "and redistribute it under the GNU General Public License version 3 or later.",
                 TextWrapping = TextWrapping.Wrap,
                 Width        = 318
