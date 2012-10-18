e0a903ad40ad2e3327ec3041b4deeb78194fea56
Nicolas Raoul <nicolas.raoul@gmail.com>
2012-10-19 08:45:01 +0900

    test for IBM Connections, and test runner because xUnit runner crashes for some tests

--- SparkleShare/TestLibrary/ConnectionTests.cs
+++ SparkleShare/TestLibrary/ConnectionTests.cs
@@ -6,6 +6,9 @@ using System.Text;
 using Xunit;
 using SparkleLib.Cmis;
 using SparkleLib;
+using DotCMIS;
+using DotCMIS.Client.Impl;
+using DotCMIS.Client;
 
 namespace TestLibrary
 {
@@ -15,7 +18,7 @@ namespace TestLibrary
 
         public ConnectionTests()
         {
-            SparkleConfig.DefaultConfig = new SparkleConfig("C:\\Users\\nico\\AppData\\Roaming\\cmissync", "config.xml");
+            SparkleConfig.DefaultConfig = new SparkleConfig(@"C:\Users\nico\AppData\Roaming\cmissync", "config.xml");
             testServers = new List<TestServer>();
             // Add your CMIS test server(s) below
             // testServers.Add(new TestServer("unittest0", "/localPath", "/remotePath", "http://servername:port/path", "username", "password", "repository"));
@@ -25,6 +28,14 @@ namespace TestLibrary
         public void Placebo()
         {
             Assert.Equal(4, 2 + 2);
+        }
+
+        [Fact]
+        public void TruncatePath()
+        {
+            CmisDatabase db = new CmisDatabase(@"C:\User Homes\nico\CmisSync\myfolder");
+            string shortened = db.Normalize(@"C:\User Homes\nico\CmisSync\myfolder\dir1\dir2\file.txt");
+            Assert.Equal("dir1/dir2/file.txt", shortened);
         }
 
         [Fact]
@@ -52,6 +63,40 @@ namespace TestLibrary
                 );
                 cmis.Sync();
             });
+        }
+
+        [Fact]
+        public void DotCmisToIBMConnections()
+        {
+            var cmisParameters = new Dictionary<string, string>();
+            cmisParameters[SessionParameter.BindingType] = BindingType.AtomPub;
+            cmisParameters[SessionParameter.AtomPubUrl] = "https://greenhouse.lotus.com/files/basic/cmis/my/servicedoc";
+            cmisParameters[SessionParameter.User] = "get one at greenhouse.lotus.com";
+            cmisParameters[SessionParameter.Password] = "get one at greenhouse.lotus.com";
+
+            SessionFactory factory = SessionFactory.NewInstance();
+            ISession session = factory.GetRepositories(cmisParameters)[0].CreateSession();
+
+            Console.WriteLine("Depth: 1");
+            IFolder root = session.GetRootFolder();
+            IItemEnumerable<ICmisObject> children = root.GetChildren();
+            foreach (var folder in children.OfType<IFolder>())
+            {
+                Console.WriteLine(folder.Path);
+            }
+
+            Console.WriteLine("Depth: 2");
+            root = session.GetRootFolder();
+            children = root.GetChildren();
+            foreach (var folder in children.OfType<IFolder>())
+            {
+                Console.WriteLine(folder.Path);
+                IItemEnumerable<ICmisObject> subChildren = folder.GetChildren();
+                foreach (var subFolder in subChildren.OfType<IFolder>()) // Exception happens here, see https://issues.apache.org/jira/browse/CMIS-593
+                {
+                    Console.WriteLine(subFolder.Path);
+                }
+            }
         }
     }
 
---
+++ SparkleShare/TestLibraryRunner/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TestLibrary;
+
+namespace TestLibraryRunner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new ConnectionTests().DotCmisToIBMConnections();
+
+#if DEBUG
+            Console.WriteLine("Press any key to close...");
+            Console.ReadLine();
+#endif
+        }
+    }
+}
