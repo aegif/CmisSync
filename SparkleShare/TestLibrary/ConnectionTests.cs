8f89b058b80a0028b4cb22e470444cda66778cda
Nicolas Raoul <nicolas.raoul@gmail.com>
2012-11-13 10:51:29 +0900

    test parameters

--- SparkleShare/TestLibrary/ConnectionTests.cs
+++ SparkleShare/TestLibrary/ConnectionTests.cs
@@ -24,9 +24,7 @@ namespace TestLibrary
             testServers = new List<TestServer>();
             // Add your CMIS test server(s) below
             // testServers.Add(new TestServer("unittest0", "/localPath", "/remotePath", "http://servername:port/path", "username", "password", "repository"));
-            testServers.Add(new TestServer("unittest1", "/mftArchive", "/mftArchive", "http://ibmcmis.dyndns.org:8080/p8cmis/resources/Service", "CEMPAdmin", "CEMPAdmin", "DaphneA"));
-            testServers.Add(new TestServer("unittest2", "/Test1", "/Test1", "http://ibmcmis.dyndns.org:8080/p8cmis/resources/Service", "CEMPAdmin", "CEMPAdmin", "DaphneB"));
-            testServers.Add(new TestServer("unittest3", "/User Homes", "/User Homes", "http://avenue.aegif.jp/alfresco/cmisatom", "nicolas.raoul", "eR31g6HG", "676bbe8a-b507-419e-99c7-8612a9fead59"));
+            // TODO 1
         }
 
         public void Dispose()
@@ -117,10 +115,7 @@ namespace TestLibrary
         {
             var cmisParameters = new Dictionary<string, string>();
             cmisParameters[SessionParameter.BindingType] = BindingType.AtomPub;
-            cmisParameters[SessionParameter.AtomPubUrl] = "http://ibmcmis.dyndns.org:8080/p8cmis/resources/Service";
-            cmisParameters[SessionParameter.User] = "CEMPAdmin";
-            cmisParameters[SessionParameter.Password] = "CEMPAdmin";
-            cmisParameters[SessionParameter.RepositoryId] = "DaphneB";
+            // TODO 2
 
             SessionFactory factory = SessionFactory.NewInstance();
             IList<IRepository> repositories = factory.GetRepositories(cmisParameters);
@@ -135,9 +130,7 @@ namespace TestLibrary
         {
             var cmisParameters = new Dictionary<string, string>();
             cmisParameters[SessionParameter.BindingType] = BindingType.AtomPub;
-            cmisParameters[SessionParameter.AtomPubUrl] = "https://greenhouse.lotus.com/files/basic/cmis/my/servicedoc";
-            cmisParameters[SessionParameter.User] = "nicolas.raoul@aegif.jp";
-            cmisParameters[SessionParameter.Password] = "notanota";
+            // TODO 3
 
             SessionFactory factory = SessionFactory.NewInstance();
             ISession session = factory.GetRepositories(cmisParameters)[0].CreateSession();
