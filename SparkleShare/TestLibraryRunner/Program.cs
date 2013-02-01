5e6c7547e18d5485589148f1ad56c4bd987c1fab
Nicolas Raoul <nicolas.raoul@gmail.com>
2013-01-31 19:31:32 -0800

    debug latest unit test

--- SparkleShare/TestLibraryRunner/Program.cs
+++ SparkleShare/TestLibraryRunner/Program.cs
@@ -18,7 +18,7 @@ namespace TestLibraryRunner
             IEnumerable<object[]> servers = JsonConvert.DeserializeObject<List<object[]>>(
                     File.ReadAllText("../../../TestLibrary/test-servers.json"));
             object[] server = servers.ElementAt(serverId);
-            new CmisSyncTests().ClientSideDirectoryAndSmallFilesAddition((string)server[0], (string)server[1],
+            new CmisSyncTests().SyncWhileModifyingFile((string)server[0], (string)server[1],
                 (string)server[2], (string)server[3], (string)server[4], (string)server[5], (string)server[6]);
 
 // Let the console open.
