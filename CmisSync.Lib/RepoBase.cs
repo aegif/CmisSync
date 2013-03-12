98c69e29112c1f8a76ff2e58cb8969f44f89a1a3
Yannick MOLINET <Yannick MOLINET>
2013-03-12 12:44:35 +0100

    Work on Bubble, only on conflict (not yet tested)

--- CmisSync.Lib/RepoBase.cs
+++ CmisSync.Lib/RepoBase.cs
@@ -287,7 +287,7 @@ namespace CmisSync.Lib
         }
 
 
-        protected void OnConflictResolved()
+        protected internal void OnConflictResolved()
         {
             ConflictResolved();
         }
