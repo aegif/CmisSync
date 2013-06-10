6f88aa361d8fc1104298d514e5c3b31c9422bc16
Achim Derigs <achim.derigs@graudata.com>
2013-06-11 00:36:11 +0200

    Enabling log4net in test runner

--- CmisSync/TestLibraryRunner/Program.cs
+++ CmisSync/TestLibraryRunner/Program.cs
@@ -6,17 +6,24 @@ using TestLibrary;
 using Newtonsoft.Json;
 using System.IO;
 
+using CmisSync.Lib;
+using CmisSync.Lib.Sync;
+
+using log4net;
+using log4net.Config;
+
 // Useful to debug unit tests.
 namespace TestLibraryRunner
 {
     class Program
     {
+        private static readonly ILog Logger = LogManager.GetLogger(typeof(Program));
         static int serverId = 0; // Which server in the JSON file (first=0)
 
-        static void test()
+        static void test(string path)
         {
             IEnumerable<object[]> servers = JsonConvert.DeserializeObject<List<object[]>>(
-                    File.ReadAllText("../../../TestLibrary/test-servers.json"));
+                    File.ReadAllText(path));
             object[] server = servers.ElementAt(serverId);
 
             new CmisSyncTests().SyncWhileModifyingFile((string)server[0], (string)server[1],
@@ -33,7 +40,29 @@ namespace TestLibraryRunner
 
         static void Main(string[] args)
         {
-            test();
+            bool firstRun = ! File.Exists(ConfigManager.CurrentConfigFile);
+
+            // Migrate config.xml from past versions, if necessary.
+            if ( ! firstRun )
+                ConfigMigration.Migrate();
+
+            // Clear log file.
+            File.Delete(ConfigManager.CurrentConfig.GetLogFilePath());
+
+            log4net.Config.XmlConfigurator.Configure(ConfigManager.CurrentConfig.GetLog4NetConfig());
+            Logger.Info("Starting.");
+            string path = null;
+
+            foreach (string arg in args)
+            {
+                 if (File.Exists(arg))
+                 {
+                     path = arg;
+                     break;
+                 }
+            }
+
+            test(path == null ? "../../../TestLibrary/test-servers.json" : path);
             //testFuzzy();
 
             // Let the console open.
