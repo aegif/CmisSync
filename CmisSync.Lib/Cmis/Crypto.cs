0c9e25937bf1ef682e172f1b677462178d0a815c
Nicolas Raoul <nicolas.raoul@gmail.com>
2013-06-12 19:18:00 +0900

    on Windows DPAPI is the usual thing for this. Also, the AES algorithm seems to always return just a newline character

--- CmisSync.Lib/Cmis/Crypto.cs
+++ CmisSync.Lib/Cmis/Crypto.cs
@@ -15,7 +15,85 @@ namespace CmisSync.Lib.Cmis
                 "Thou art so farth away, I miss you my dear files❥, with CmisSync be forever by my side!");
         }
 
+        // Obfuscate a string.
         public static string Protect(string value)
+        {
+            if (Environment.OSVersion.Platform == PlatformID.Win32NT)
+            {
+                return WindowsProtect(value);
+            }
+            else
+            {
+                return UnixProtect(value);
+            }
+        }
+
+        // De-obfuscate a string.
+        public static string Unprotect(string value)
+        {
+            if (Environment.OSVersion.Platform == PlatformID.Win32NT)
+            {
+                return WindowsUnprotect(value);
+            }
+            else
+            {
+                return UnixUnprotect(value);
+            }
+        }
+
+        // On Windows, we use the recommended DPAPI
+        public static string WindowsProtect(string value)
+        {
+            #if __MonoCS__
+                return "Should never be reached";
+            #else
+                try
+                {
+                    byte[] data = System.Text.Encoding.UTF8.GetBytes(value);
+                    // Encrypt the data using DataProtectionScope.CurrentUser. The result can be decrypted
+                    //  only by the same current user.
+                    byte[] crypt = ProtectedData.Protect(data, GetCryptoKey(), DataProtectionScope.CurrentUser);
+                    return Convert.ToBase64String(crypt, Base64FormattingOptions.None);
+                }
+                catch (CryptographicException e)
+                {
+                    Console.WriteLine("Data was not encrypted. An error occurred.");
+                    Console.WriteLine(e.ToString());
+                    return null;
+                }
+            #endif
+        }
+
+        public static string WindowsUnprotect(string value)
+        {
+            #if __MonoCS__
+                return "Should never be reached";
+            #else
+                try
+                {
+                    byte[] data = Convert.FromBase64String(value);
+                    //Decrypt the data using DataProtectionScope.CurrentUser.
+                    byte[] uncrypt = ProtectedData.Unprotect(data, GetCryptoKey(), DataProtectionScope.CurrentUser);
+                    return System.Text.Encoding.UTF8.GetString(uncrypt);
+                }
+                catch (Exception e)
+                {
+                    if (e is CryptographicException || e is FormatException)
+                    {
+                        Console.WriteLine("Your password is not obfuscated yet.");
+                        Console.WriteLine("Using unobfuscated value directly might be deprecated soon, so please delete your local directories and recreate them. Thank you for your understanding.");
+                        return value;
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+            #endif
+        }
+
+        
+        public static string UnixProtect(string value)
         {
             try
             {
@@ -40,7 +118,7 @@ namespace CmisSync.Lib.Cmis
             }
         }
 
-        public static string Unprotect(string value)
+        public static string UnixUnprotect(string value)
         {
             try
             {
