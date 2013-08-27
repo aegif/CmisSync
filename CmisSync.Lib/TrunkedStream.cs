f8f18a13d529200945b269ad71abc0067f5ad6f3
more0401 <more0401@gmail.com>
2013-08-27 12:02:47 +0800

    Add empty CmisSync.Lib.TrunkedStream

---
+++ CmisSync.Lib/TrunkedStream.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace CmisSync.Lib
+{
+    class TrunkedStream : Stream
+    {
+        private Stream source;
+        private string sourceName;
+        private Cmis.Database sourceDatabase;
+        private long trunkSize;
+
+        public TrunkedStream(Stream stream, string name, Cmis.Database database, long trunk)
+        {
+            source = stream;
+            sourceName = name;
+            sourceDatabase = database;
+            trunkSize = trunk;
+
+            if (!source.CanRead)
+            {
+                throw new System.NotSupportedException("Read access is needed for TrunkedStream");
+            }
+        }
+
+        public override bool CanRead { get { return source.CanRead; } }
+        public override bool CanWrite { get { return source.CanWrite; } }
+        public override bool CanSeek { get { return source.CanSeek; } }
+        public override void Flush() { source.Flush(); }
+
+        public override long Length
+        {
+            get
+            {
+                Debug.Assert(false, "TODO");
+                return source.Length;
+            }
+        }
+
+        public override long Position
+        {
+            get
+            {
+                Debug.Assert(false, "TODO");
+                return source.Position;
+            }
+
+            set
+            {
+                Debug.Assert(false, "TODO");
+                source.Position = value;
+            }
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            Debug.Assert(false, "TODO");
+            return source.Read(buffer, offset, count);
+        }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            Debug.Assert(false, "TODO");
+            source.Write(buffer, offset, count);
+        }
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            Debug.Assert(false, "TODO");
+            return source.Seek(offset, origin);
+        }
+
+        public override void SetLength(long value)
+        {
+            Debug.Assert(false, "TODO");
+            source.SetLength(value);
+        }
+    }
+}
---
+++ CmisSync/TestLibrary/TrunkedStreamTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+using NUnit.Framework;
+
+
+namespace TestLibrary
+{
+    [TestFixture]
+    class TrunkedStreamTest
+    {
+        [Test]
+        public void TestWrite()
+        {
+            Assert.Fail("TODO");
+        }
+
+        [Test]
+        public void TestRead()
+        {
+            Assert.Fail("TODO");
+        }
+
+        [Test]
+        public void TestWriteResume()
+        {
+            Assert.Fail("TODO");
+        }
+
+        [Test]
+        public void TestReadResume()
+        {
+            Assert.Fail("TODO");
+        }
+    }
+
+
+
+
+}
