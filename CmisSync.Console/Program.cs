4277e54864b36ab44ac7c734c53ec98648db7f19
more0401 <more0401@gmail.com>
2013-09-09 17:23:11 +0800

    Initial console client codes

---
+++ CmisSync.Console/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+using CmisSync.Lib;
+
+
+namespace CmisSync.Console
+{
+    class ActivityListener : IActivityListener
+    {
+        public void ActivityStarted()
+        {
+        }
+
+        public void ActivityStopped()
+        {
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<RepoBase> repositories = new List<RepoBase>();
+
+            foreach (Config.SyncConfig.Folder folder in ConfigManager.CurrentConfig.Folder)
+            {
+                string path = folder.LocalPath;
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+
+                RepoBase repo = new CmisSync.Lib.Sync.CmisRepo(folder.GetRepoInfo(),new ActivityListener());
+                repositories.Add(repo);
+                repo.Initialize();
+            }
+
+            while(true)
+            {
+                System.Threading.Thread.Sleep(1);
+            }
+        }
+    }
+}
