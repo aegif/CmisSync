01db23942e73d8d7ff4f9a52ead0ec7c1e4b4d9b
Till Lorentzen <till.lorentzen@graudata.com>
2013-09-10 18:52:53 +0200

    Added AssemblyInfo

--- DataSpaceSync.Console/Properties/AssemblyInfo.cs
+++ DataSpaceSync.Console/Properties/AssemblyInfo.cs
@@ -1,15 +1,17 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CmisSync.Console")]
-[assembly: AssemblyDescription("")]
+[assembly: CLSCompliant(true)]
+[assembly: AssemblyTitle("DataSpaceSync.Console")]
+[assembly: AssemblyDescription("Console version of the DataSpace Sync Client")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CmisSync.Console")]
+[assembly: AssemblyCompany("GRAU DATA AG")]
+[assembly: AssemblyProduct("DataSpaceSync.Console")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
