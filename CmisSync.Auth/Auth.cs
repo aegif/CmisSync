4ac7c9af20c163e55fdab5fc7d49b92d4ca9bf63
Nicolas Raoul <nicolas.raoul@gmail.com>
2013-11-28 20:39:46 +0900

    consistency

--- CmisSync.Auth/Auth.cs
+++ CmisSync.Auth/Auth.cs
@@ -21,19 +21,19 @@ namespace CmisSync.Auth
             // Create session factory.
             SessionFactory factory = SessionFactory.NewInstance();
 
-            return factory.GetRepositories(parameters); ;
+            return factory.GetRepositories(parameters);
         }
 
         public static ISession GetCmisSession(string url, string repositoryId, string user, string password)
         {
-            Dictionary<string, string> cmisParameters = new Dictionary<string, string>();
-            cmisParameters[SessionParameter.BindingType] = BindingType.AtomPub;
-            cmisParameters[SessionParameter.AtomPubUrl] = url;
-            cmisParameters[SessionParameter.User] = user;
-            cmisParameters[SessionParameter.Password] = password;
-            cmisParameters[SessionParameter.RepositoryId] = repositoryId;
+            Dictionary<string, string> parameters = new Dictionary<string, string>();
+            parameters[SessionParameter.BindingType] = BindingType.AtomPub;
+            parameters[SessionParameter.AtomPubUrl] = url;
+            parameters[SessionParameter.User] = user;
+            parameters[SessionParameter.Password] = password;
+            parameters[SessionParameter.RepositoryId] = repositoryId;
             SessionFactory factory = SessionFactory.NewInstance();
-            return factory.CreateSession(cmisParameters);
+            return factory.CreateSession(parameters);
         }
     }
 }
