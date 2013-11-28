bd00dbb695beb1f04a793c8e2d2044bffbc9b12e
Keith Harrison <keithharrison@oris4.com>
2013-11-28 16:36:16 -0400

    updated installer configuration to build installer for Release configuration

--- CmisSync/Windows/build.cmd
+++ CmisSync/Windows/build.cmd
@@ -10,16 +10,16 @@ set wixBinDir=%WIX%\bin
 if not exist ..\..\bin mkdir ..\..\bin
 copy Pixmaps\cmissync-app.ico ..\..\bin\
 
-%msbuild% /t:Clean,Build /p:Configuration=Debug /p:Platform="Any CPU" "%~dp0\CmisSync.sln"
+%msbuild% /t:Clean,Build /p:Configuration=Release /p:Platform="Any CPU" "%~dp0\CmisSync.sln"
 
 if "%1"=="installer" (
 	if exist "%wixBinDir%" (
-	  if exist "%~dp0\Oris4Sync.msi" del "%~dp0\Oris4Sync.msi"
+	  if exist "%~dp0\..\..\bin\Oris4Sync.msi" del "%~dp0\..\..\bin\Oris4Sync.msi"
 		"%wixBinDir%\heat.exe" dir "%~dp0\..\..\bin\plugins" -cg pluginsComponentGroup -gg -scom -sreg -sfrag -srd -dr PLUGINS_DIR -var wix.pluginsdir -o plugins.wxs
 		"%wixBinDir%\candle" "%~dp0\CmisSync.wxs" -ext WixUIExtension -ext WixUtilExtension -ext WiXNetFxExtension
 		"%wixBinDir%\candle" "%~dp0\plugins.wxs" -ext WixUIExtension -ext WixUtilExtension -ext WiXNetFxExtension
-		"%wixBinDir%\light" -ext WixUIExtension -ext WixUtilExtension -ext WiXNetFxExtension -cultures:en-us CmisSync.wixobj plugins.wixobj -droot="%~dp0\..\.." -dpluginsdir="%~dp0\..\..\bin\plugins"  -o Oris4Sync.msi 
-		if exist "%~dp0\Oris4Sync.msi" echo Oris4Sync.msi created.
+		"%wixBinDir%\light" -ext WixUIExtension -ext WixUtilExtension -ext WiXNetFxExtension -cultures:en-us CmisSync.wixobj plugins.wixobj -droot="%~dp0\..\.." -dpluginsdir="%~dp0\..\..\bin\plugins"  -o "%~dp0\..\..\bin\Oris4Sync.msi" 
+		if exist "%~dp0\..\..\bin\Oris4Sync.msi" echo Oris4Sync.msi created.
 	) else (
 		echo Not building installer ^(could not find wix, Windows Installer XML toolset^)
 		echo wix is available at http://wix.sourceforge.net/
