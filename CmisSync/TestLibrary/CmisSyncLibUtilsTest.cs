0ba6b7ebf030122b708092990af84f27f0005976
Till Lorentzen <till.lorentzen@graudata.com>
2013-12-10 17:16:42 +0100

    Added test libraries to linux sln

--- CmisSync/TestLibrary/CmisSyncLibUtilsTest.cs
+++ CmisSync/TestLibrary/CmisSyncLibUtilsTest.cs
@@ -1,4 +1,4 @@
-﻿﻿using System;
+﻿using System;
 using System.IO;
 using CmisSync.Lib;
 using NUnit.Framework;
@@ -52,7 +52,9 @@ namespace TestLibrary
                 Assert.AreNotEqual(path, conflictFilePath, "The conflict file must differ from original file");
                 Assert.True(conflictFilePath.Contains(user), "The username should be added to the conflict file name");
                 Assert.True(conflictFilePath.EndsWith(Path.GetExtension(path)), "The file extension must be kept the same as in the original file");
-//                string filename = Path.GetFileName(conflictFilePath);
+                string filename = Path.GetFileName(conflictFilePath);
+                string originalFilename = Path.GetFileNameWithoutExtension(path);
+                Assert.True(filename.StartsWith(originalFilename), String.Format("The conflict file \"{0}\" must start with \"{1}\"", filename, originalFilename));
                 string conflictParent = Directory.GetParent(conflictFilePath).FullName;
                 Assert.AreEqual(originalParent, conflictParent, "The conflict file must exists in the same directory like the orignial file");
             }
