f8f2c77aed28a072e2432f1069ecbcdce84c69b0
Keith Harrison <keithharrison@oris4.com>
2013-12-31 10:30:38 -0400

    Minor vix to file names and build script

--- CmisSync/Windows/build.cmd
+++ CmisSync/Windows/build.cmd
@@ -5,8 +5,3 @@ set msbuild="%WinDirNet%\v4.0\msbuild.exe"
 if not exist %msbuild% set msbuild="%WinDirNet%\v4.0.30319\msbuild.exe"
 
 %msbuild% /t:Clean,Build /p:Configuration=Release /p:Platform="Any CPU" "%~dp0\CmisSync.sln"
-
-if exist "%~dp0\InstallerBootstrapper\bin\Release\Oris4Sync.exe" (
-	if not exist "%~dp0\..\..\bin" mkdir "%~dp0\..\..\bin"
-	copy "%~dp0\InstallerBootstrapper\bin\Release\Oris4Sync.exe" "%~dp0\..\..\bin"
-) else echo "Failed to build Oris4Sync.exe installer"
