7008b728486cf1adbf34d4b8d83121e9bf5de821
Nicolas Raoul <nicolas.raoul@gmail.com>
2014-01-10 16:04:48 +0900

    Bumped version 1.1

--- CmisSync.Lib/Properties/AssemblyInfo.cs
+++ CmisSync.Lib/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("CmisSync.Lib")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.14.*")]
+[assembly: AssemblyVersion("1.1")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
