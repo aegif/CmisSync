9b1ab1bbf6276272c3fe95dcdfca91d5402c6bc7
Till Lorentzen <till.lorentzen@graudata.com>
2014-01-30 15:25:07 +0100

    Fixed the same possible thread issue for MacOS

--- CmisSync/Mac/CertPolicyWindow.cs
+++ CmisSync/Mac/CertPolicyWindow.cs
@@ -1,8 +1,7 @@
 using System;
-
+using System.Threading;
 using MonoMac.Foundation;
 using MonoMac.AppKit;
-
 using log4net;
 
 namespace CmisSync
@@ -13,36 +12,53 @@ namespace CmisSync
 
         private CertPolicyHandler Handler { get; set; }
 
-        public CertPolicyWindow (CertPolicyHandler handler)
+        public CertPolicyWindow(CertPolicyHandler handler)
         {
             Handler = handler;
             Handler.ShowWindowEvent += ShowCertDialog;
         }
 
-        private void ShowCertDialog() {
+        private void ShowCertDialog()
+        {
             Logger.Debug("Showing Cert Dialog: " + Handler.UserMessage);
             CertPolicyHandler.Response ret = CertPolicyHandler.Response.None;
-            InvokeOnMainThread(delegate {
-                NSAlert alert = NSAlert.WithMessage("Untrusted Certificate", "No", "Always", "Just now", Handler.UserMessage + "\n\nDo you trust this certificate?");
-                switch (alert.RunModal()) {
-                case 1:
-                    ret = CertPolicyHandler.Response.CertDeny;
-                    break;
-                case 0:
-                    ret = CertPolicyHandler.Response.CertAcceptAlways;
-                    break;
-                case -1:
-                    ret = CertPolicyHandler.Response.CertAcceptSession;
-                    break;
-                default:
-                    ret = CertPolicyHandler.Response.None;
-                    break;
-                }
-            });
+            using (var signal = new AutoResetEvent(false))
+            {
+                InvokeOnMainThread(delegate
+                {
+                    try
+                    {
+                        NSAlert alert = NSAlert.WithMessage("Untrusted Certificate", "No", "Always", "Just now", Handler.UserMessage + "\n\nDo you trust this certificate?");
+                        switch (alert.RunModal())
+                        {
+                            case 1:
+                                ret = CertPolicyHandler.Response.CertDeny;
+                                break;
+                            case 0:
+                                ret = CertPolicyHandler.Response.CertAcceptAlways;
+                                break;
+                            case -1:
+                                ret = CertPolicyHandler.Response.CertAcceptSession;
+                                break;
+                            default:
+                                ret = CertPolicyHandler.Response.None;
+                                break;
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        ret = CertPolicyHandler.Response.None;
+                    }
+                    finally
+                    {
+                        signal.Set();
+                    }
+                });
+                signal.WaitOne();
+            }
             Logger.Debug("Cert Dialog return:" + ret.ToString());
             Handler.UserResponse = ret;
         }
-
     }
 }
 
