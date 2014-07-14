d4940d2c7f2374c9dcf9b781fe6dc5587b84ffc2
Nicolas Raoul <nicolas.raoul@gmail.com>
2014-07-14 18:10:29 +0900

    dont declare until needed

--- CmisSync.Lib/Cmis/Database.cs
+++ CmisSync.Lib/Cmis/Database.cs
@@ -137,46 +137,46 @@ namespace CmisSync.Lib.Cmis
                     sqliteConnection = new SQLiteConnection("Data Source=" + databaseFileName + ";PRAGMA journal_mode=WAL");
                     sqliteConnection.Open();
 
-                    string command =
-                   @"CREATE TABLE IF NOT EXISTS files (
-                        path TEXT PRIMARY KEY,
-                        localPath TEXT, /* Local path is sometimes different due to local filesystem constraints */
-                        id TEXT,
-                        serverSideModificationDate DATE,
-                        metadata TEXT,
-                        checksum TEXT);   /* Checksum of both data and metadata */
-                    CREATE INDEX IF NOT EXISTS files_localPath_index ON files (localPath);
-                    CREATE INDEX IF NOT EXISTS files_id_index ON files (id);
-                    CREATE TABLE IF NOT EXISTS folders (
-                        path TEXT PRIMARY KEY,
-                        localPath TEXT, /* Local path is sometimes different due to local filesystem constraints */
-                        id TEXT,
-                        serverSideModificationDate DATE,
-                        metadata TEXT,
-                        checksum TEXT);   /* Checksum of metadata */
-                    CREATE INDEX IF NOT EXISTS folders_localPath_index ON folders (localPath);
-                    CREATE INDEX IF NOT EXISTS folders_id_index ON folders (id);
-                    CREATE TABLE IF NOT EXISTS general (
-                        key TEXT PRIMARY KEY,
-                        value TEXT);      /* Other data such as ChangeLog token */
-                    CREATE TABLE IF NOT EXISTS downloads (
-                        PATH TEXT PRIMARY KEY,
-                        serverSideModificationDate DATE);     /* Download */
-                    CREATE TABLE IF NOT EXISTS failedoperations (
-                        path TEXT PRIMARY KEY,
-                        lastLocalModificationDate DATE,
-                        uploadCounter INTEGER,
-                        downloadCounter INTEGER,
-                        changeCounter INTEGER,
-                        deleteCounter INTEGER,
-                        uploadMessage TEXT,
-                        downloadMessage TEXT,
-                        changeMessage TEXT,
-                        deleteMessage TEXT);     /* Failed Operations*/
-                    DROP TABLE IF EXISTS faileduploads; /* Drop old upload Counter Table*/";
-
                     if (createDatabase)
                     {
+                        string command =
+                       @"CREATE TABLE IF NOT EXISTS files (
+                            path TEXT PRIMARY KEY,
+                            localPath TEXT, /* Local path is sometimes different due to local filesystem constraints */
+                            id TEXT,
+                            serverSideModificationDate DATE,
+                            metadata TEXT,
+                            checksum TEXT);   /* Checksum of both data and metadata */
+                        CREATE INDEX IF NOT EXISTS files_localPath_index ON files (localPath);
+                        CREATE INDEX IF NOT EXISTS files_id_index ON files (id);
+                        CREATE TABLE IF NOT EXISTS folders (
+                            path TEXT PRIMARY KEY,
+                            localPath TEXT, /* Local path is sometimes different due to local filesystem constraints */
+                            id TEXT,
+                            serverSideModificationDate DATE,
+                            metadata TEXT,
+                            checksum TEXT);   /* Checksum of metadata */
+                        CREATE INDEX IF NOT EXISTS folders_localPath_index ON folders (localPath);
+                        CREATE INDEX IF NOT EXISTS folders_id_index ON folders (id);
+                        CREATE TABLE IF NOT EXISTS general (
+                            key TEXT PRIMARY KEY,
+                            value TEXT);      /* Other data such as ChangeLog token */
+                        CREATE TABLE IF NOT EXISTS downloads (
+                            PATH TEXT PRIMARY KEY,
+                            serverSideModificationDate DATE);     /* Download */
+                        CREATE TABLE IF NOT EXISTS failedoperations (
+                            path TEXT PRIMARY KEY,
+                            lastLocalModificationDate DATE,
+                            uploadCounter INTEGER,
+                            downloadCounter INTEGER,
+                            changeCounter INTEGER,
+                            deleteCounter INTEGER,
+                            uploadMessage TEXT,
+                            downloadMessage TEXT,
+                            changeMessage TEXT,
+                            deleteMessage TEXT);     /* Failed Operations*/
+                        DROP TABLE IF EXISTS faileduploads; /* Drop old upload Counter Table*/";
+
                         ExecuteSQLAction(command, null);
                         Logger.Info("Database created");
                     }
@@ -779,6 +779,7 @@ namespace CmisSync.Lib.Cmis
             parameters.Add("path", normalizedPath);
             previousChecksum = (string)ExecuteSQLFunction(command, parameters);
 
+            // Compare checksums.
             if (!currentChecksum.Equals(previousChecksum))
                 Logger.Info("Checksum of " + path + " has changed from " + previousChecksum + " to " + currentChecksum);
             return !currentChecksum.Equals(previousChecksum);
