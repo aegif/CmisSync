634e8d786ebd2b442ef6bf94a5de1ce8a1f21ac7
Nicolas Raoul <nicolas.raoul@gmail.com>
2014-09-04 12:27:05 +0900

    comment

--- CmisSync.Lib/Sync/SynchronizedFolder.cs
+++ CmisSync.Lib/Sync/SynchronizedFolder.cs
@@ -1273,7 +1273,7 @@ namespace CmisSync.Lib.Sync
             /// <summary>
             /// Upload a single file to the CMIS server.
             /// </summary>
-            private bool UploadFile(string filePath, IFolder remoteFolder)
+            private bool UploadFile(string filePath, IFolder remoteFolder) // TODO make SyncItem
             {
                 SleepWhileSuspended();
 
