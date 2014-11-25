781d3b72392c407f77826c7be721660f320cccb0
Nicolas Raoul <nicolas.raoul@gmail.com>
2014-11-25 17:43:05 +0900

    bumped 2.0.10

--- CmisSync.Lib/Properties/AssemblyInfo.cs
+++ CmisSync.Lib/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("562D25D1-4006-44FD-9580-F8F4A6F3D1B1")]
+[assembly: Guid("662D25D1-4006-44FD-9580-F8F4A6F3D1B1")]
 
 
 // Version information for an assembly consists of the following four values:
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.0.9")]
+[assembly: AssemblyVersion("2.0.10")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
