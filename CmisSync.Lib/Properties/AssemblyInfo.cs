8ccd3480a37c2c33827c3fb7f1db6fc0278278f3
Nicolas Raoul <nicolas.raoul@gmail.com>
2018-06-11 02:19:03 -0700

    Bumped version to 2.12.1 after upgrading to Visual Studio 2015

--- CmisSync.Lib/Properties/AssemblyInfo.cs
+++ CmisSync.Lib/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("CmisSync.Lib")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("21a6847a-d4c2-4806-87a1-fda13a9ee6e1")]
+[assembly: Guid("E52A2D2E-DCAC-44A0-BCA5-D0D83FE5C618")]
 
 
 // Version information for an assembly consists of the following four values:
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.12.0")]
+[assembly: AssemblyVersion("2.12.1")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
